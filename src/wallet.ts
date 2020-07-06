4bfe4a879a3376d650aaedd7cb90bd26edaa7b9b
pefish <pefish@qq.com>
2020-07-06 11:07:09 +0800

    add

--- src/wallet.ts
+++ src/wallet.ts
@@ -5,26 +5,31 @@ import ErrorHelper from '@pefish/js-error';
 import IOST from 'iost/index'
 import bs58 from 'bs58'
 
+interface Option {
+  gasRatio: number,
+  gasLimit: number,
+  delay: number,
+  expiration: number,
+  url: string,
+}
 export default class wallet {
   private wallet: IOST.IOST
   private remote: IOST.RPC
 
-  constructor () {
-    this.wallet = new IOST.IOST({
-      gasRatio: 1,
-      gasLimit: 200000,
-      delay:0,
-      expiration: 90,
-    });
-    this.remote = new IOST.RPC(new IOST.HTTPProvider('http://api.iost.io'));
+  constructor (opts: Option = {
+    gasRatio: 1,
+    gasLimit: 200000,
+    delay:0,
+    expiration: 90,
+    url: 'http://api.iost.io',
+  }) {
+    this.wallet = new IOST.IOST(opts);
+    this.remote = new IOST.RPC(new IOST.HTTPProvider(opts.url));
     this.wallet.setRPC(this.remote);
   }
 
   getAllBySeedAndIndex(seed: string, index: number) {
-    if (seed.length !== 20) {
-      throw new ErrorHelper(`length of seed must be 20`)
-    }
-    const newSeed = seed + index.toString().padStart(12, `0`)
+    const newSeed = seed.padStart(20, `0`) + index.toString().padStart(12, `0`)
     const kp1 = nacl.sign.keyPair.fromSeed(newSeed.toBuffer_());
     const kp2 = new KeyPair(Buffer.from(kp1.secretKey.buffer));
     return {
--- types/wallet.d.ts
+++ types/wallet.d.ts
@@ -1,11 +1,19 @@
 import '@pefish/js-node-assist';
+interface Option {
+    gasRatio: number;
+    gasLimit: number;
+    delay: number;
+    expiration: number;
+    url: string;
+}
 export default class wallet {
     private wallet;
     private remote;
-    constructor();
+    constructor(opts?: Option);
     getAllBySeedAndIndex(seed: string, index: number): {
         privateKey: any;
         publicKey: any;
     };
     sendContractTx(fromAccount: string, pkey: string, contractAddress: string, method: string, params: any[]): Promise<any>;
 }
+export {};
